367a4ff762c84b4c140205ee74fa5efd835c95f3
Василий Яковлев <vasya_anime@mail.ru>
2020-09-21 17:51:18 +0500

    Добавьте файлы проекта.

---
+++ СИАКОД_4_0/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace СИАКОД_4_0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pBox = new System.Windows.Forms.PictureBox();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox)).BeginInit();
+            this.groupBox.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pBox
+            // 
+            this.pBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pBox.Location = new System.Drawing.Point(125, 0);
+            this.pBox.Name = "pBox";
+            this.pBox.Size = new System.Drawing.Size(675, 332);
+            this.pBox.TabIndex = 0;
+            this.pBox.TabStop = false;
+            this.pBox.Paint += new System.Windows.Forms.PaintEventHandler(this.pBox_Paint);
+            this.pBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pBox_MouseClick);
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.button2);
+            this.groupBox.Controls.Add(this.button1);
+            this.groupBox.Location = new System.Drawing.Point(0, 0);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(125, 436);
+            this.groupBox.TabIndex = 1;
+            this.groupBox.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(25, 81);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Очищение";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "BrAlgo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(125, 331);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(496, 107);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(483, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Вариант 16. В заданном графе найти максимальный по количеству вершин полный подгр" +
+    "аф.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Клика:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox);
+            this.Controls.Add(this.pBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pBox)).EndInit();
+            this.groupBox.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pBox;
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ СИАКОД_4_0/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace СИАКОД_4_0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Node> nodes = new List<Node>();
+        List<Rebro> rebra = new List<Rebro>();
+        //public List<Node> nodes = new List<Node>();
+        int globalflag = 0;
+        private void pBox_Paint(object sender, PaintEventArgs e)
+        {
+            Pen pen;
+            pen = new Pen(Brushes.Blue);
+            pen.Width = 4;
+            if (globalflag == 1)
+            {
+                foreach (Node v in maxR)
+                {
+                    for (int i = 0; i < maxR.Count; i++)
+                    {
+                        e.Graphics.DrawLine(pen, v.x, v.y, maxR[i].x, maxR[i].y);
+                    }
+                }
+            }
+            foreach (Rebro r in rebra)
+            {
+                r.draw(e.Graphics);
+            }
+            foreach (Node n in nodes)
+            {
+               n.draw(e.Graphics);
+            }
+            
+            
+            pBox.Select();
+        }
+
+        void link(Node n1,Node n2)
+        {
+            Rebro r = new Rebro(n1, n2);
+            rebra.Add(r);
+            n1.linkedN.Add(n2);
+            n2.linkedN.Add(n1);
+            foreach (Node no in nodes)
+             {
+                if (no.CheckSelect())
+                { 
+                    no.Select();
+                }
+             }
+        }
+
+        int next = 0;
+        Node n1;
+        Node n2;
+        int count = 0;
+        private void pBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            int flag = 0;
+
+            if (next == 0)
+            {
+                foreach (Node n in nodes)
+                {
+                    if (n.mouseInShape(e.X, e.Y) == true)
+                    {
+                        next++;
+                        flag = 1;
+                        n.Select();
+                        n1 = n;
+                        this.Refresh();
+                        pBox.Select();
+                        return;
+                    }
+                }
+            }
+
+            if (next == 1)
+            {
+                foreach (Node n in nodes)
+                {
+                    if (n.mouseInShape(e.X, e.Y) == true)
+                    {
+                        flag = 1;
+                        if (n.CheckSelect() != true)
+                        {
+                            next = 0;
+                            n.Select();
+                            n2 = n;
+                           
+                            link(n1, n2);
+                            this.Refresh();
+                            pBox.Select();
+                            return;
+                           
+                        }
+                    }
+                }
+            }
+
+            if (flag == 0)
+            {
+                Node n = new Node(e.X, e.Y, count);
+                nodes.Add(n);
+                count++;
+            }
+            this.Refresh();
+            pBox.Select();
+        }
+
+        public void Wait(double seconds)
+        {
+            int ticks = System.Environment.TickCount + (int)Math.Round(seconds * 1000.0);
+            while (System.Environment.TickCount < ticks)
+            {
+                Application.DoEvents();
+            }
+        }
+
+
+
+        /////////////РГР//////////////// 
+
+         Node[] P; //все вершины
+         List<Node> R;  //множество, содержащее на каждом шаге рекурсии полный подграф для данного шага. Строится рекурсивно.
+        List<Node> X; //множество вершин, которые уже использовались для расширения
+        List<Node> maxR; // максимальная клика
+
+
+        public void bralgo(Graphics g)
+        {
+            globalflag = 0;
+            // Инициализация
+            P = new Node[nodes.Count];
+            nodes.CopyTo(P);
+            R = new List<Node>();
+            X = new List<Node>();
+            maxR = new List<Node>();
+            bron(P, X); // Запускаем алгоритм
+            // Выводим вершины клики
+            foreach (Node v in maxR)
+            {
+                label1.Text += v.num.ToString() + " ";
+            }
+            // Отрисовываем вершины клики
+            foreach (Node v in maxR)
+            {
+                v.fillEllipse();
+            }
+            globalflag = 1;
+        }
+
+         void bron(Node[] Pa, List<Node> Xa)
+        {
+            if (Xa.Count == 0 && check(Pa))
+            {
+                if (R.Count > maxR.Count)
+                {
+                    maxR = R; // Если множества X и P пустые и найдена большая клика,то сохраняем её
+                }
+                R = new List<Node>();
+                return;
+            }
+            // Для каждой вершины запускаем рекурсию, пока в P не закончатся вершины
+            for (int i = 0; i < Pa.Length; ++i)
+            {               if (Pa[i] != null)
+                {
+                    R.Add(Pa[i]);
+                    bron(conj(Pa[i], Pa), conj(Pa[i], Xa)); // Запуск рекурсии с изменёнными множествами P и X
+                    Pa[i] = null;
+                    Xa.Add(Pa[i]);
+                }
+            }
+        }
+        // Изменение множества X
+        private List<Node> conj(Node v1, List<Node> list)
+        {
+            List<Node> res = new List<Node>();
+            foreach (Node ch in v1.linkedN)
+            {
+                if (list.Contains(ch))
+                    res.Add(ch);
+            }
+            list = res;
+            return list;
+        }
+        // Изменение множества P
+        private Node[] conj(Node v1, Node[] list)
+        {
+            Node[] res = new Node[list.Length];
+            for (int i = 0; i < res.Length; ++i)
+            {
+                res[i] = null;
+            }
+            foreach (Node ch in v1.linkedN)
+            {
+                if (list.Contains(ch))
+                {
+                    int i = Array.IndexOf(list, ch);
+                    res[i] = ch;
+                }
+            }
+            list = res;
+            return list;
+        }
+        // Проверка множества P на наличие ненулевых вершин
+        private bool check(Node[] A)
+        {
+            bool ch = true;
+            foreach (Node v in A)
+                if (v != null)
+                {
+                    ch = false;
+                    break;
+                }
+            return ch;
+        }
+
+        //private List<Node> haha(List<Node> a, Node v)
+        //{
+        //    List<Node> list = a;
+        //    if (!a.Contains(v))
+        //        list.Add(v);
+        //    a = list;
+        //    return a;
+        //}
+        ///////////////////КОНЕЦ РГР//////////////////////
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = pBox.CreateGraphics(); ;
+            bralgo(g);
+            this.Refresh();
+            pBox.Select();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            count = 0;
+            rebra.Clear();
+            nodes.Clear();
+            label1.Text = "";
+            globalflag = 0;
+            this.Refresh();
+        }
+
+      
+    }
+}
